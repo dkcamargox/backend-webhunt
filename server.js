5861baf20f1b88420d2ef4b6c5af39382d2b17c6
Douglas Camargo <camargo.douglas@icloud.com>
2020-02-04 17:20:22 -0300

    server changed

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const reqDir = require("require-dir");
-const process = require("process");
+// const process = require("process");
 const cors = require("cors");
 //app
 const app = express();
 app.use(express.json());
 app.use(cors());
 //db
-mongoose.connect("mongodb+srv://dkcamargox:camargo1508@cluster0-zmptz.gcp.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true });
+// mongoose.connect("mongodb+srv://dkcamargox:camargo1508@cluster0-zmptz.gcp.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true });
+mongoose.connect("localhost:27017", { useNewUrlParser: true });
 reqDir("./src/models");
 
 
 //rota
 app.use("/api" , require("./src/routes"));
 
-app.listen(process.env.PORT || 3001);
+app.listen(3001);
 
